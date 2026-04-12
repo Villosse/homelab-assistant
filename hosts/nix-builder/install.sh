165db1b8ff7a7205fe90fa0a6f3f79a72680b94b
lenny.chiadmi-delage <lenny.chiadmi-delage@epita.fr>
2026-04-12 03:35:44 +0200

    fuck cache

--- hosts/nix-builder/install.sh
+++ hosts/nix-builder/install.sh
@@ -4,8 +4,8 @@ set -e
 
 BASE_URL="https://raw.githubusercontent.com/Villosse/homelab-assistant/main/hosts/nix-builder"
 
-curl -o /etc/nixos/configuration.nix "$BASE_URL/configuration.nix"
-curl -o /etc/nixos/root.keys "$BASE_URL/root.keys"
-curl -o /etc/nixos/builder.keys "$BASE_URL/builder.keys"
+curl -o /etc/nixos/configuration.nix "$BASE_URL/configuration.nix$(date +%s)"
+curl -o /etc/nixos/root.keys "$BASE_URL/root.keys$(date +%s)"
+curl -o /etc/nixos/builder.keys "$BASE_URL/builder.keys$(date +%s)"
 
 nixos-rebuild switch
