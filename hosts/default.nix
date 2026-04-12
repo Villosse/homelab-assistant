bd16f8c8926f0ad36b4d86d40d3e394c9888fb5a
lenny.chiadmi-delage <lenny.chiadmi-delage@epita.fr>
2026-04-12 06:51:41 +0200

    new node

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,4 +2,5 @@
 {
   nix-builder = import ./nix-builder { inherit nixpkgs; };
   k3s-master = import ./k3s-master { inherit nixpkgs; };
+  k3s-worker-1 = import ./k3s-worker-1 { inherit nixpkgs; };
 }
