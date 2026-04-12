6cc106436e927acd932ddddb46ee32514966fccc
lenny.chiadmi-delage <lenny.chiadmi-delage@epita.fr>
2026-04-12 06:40:40 +0200

    toto

--- hosts/k3s-worker-1/configuration.nix
+++ hosts/k3s-worker-1/configuration.nix
@@ -4,19 +4,15 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
 
-  networking.hostName = "k3s-master";
+  networking.hostName = "k3s-worker";
   networking.useDHCP = true;
-  environment.variables.KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
 
   services.k3s = {
     enable = true;
-    role = "server";
-    extraFlags = "--disable traefik --write-kubeconfig-mode 644"; # on gérera l'ingress nous-mêmes
+    role = "agent";
+    serverAddr = "https://192.168.2.153:6443";
+    tokenFile = /etc/k3s-token;
   };
 
   services.tailscale.enable = true;
@@ -34,11 +30,9 @@
     openssh.authorizedKeys.keyFiles = [ ./root.keys ];
   };
 
-  nix.settings.trusted-users = [ "root" ];
-
-  environment.systemPackages = with pkgs; [
-    kubectl
-    k9s
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
   ];
 
   system.stateVersion = "24.11";
