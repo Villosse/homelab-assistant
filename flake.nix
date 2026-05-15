550fd253001bffbc643c05c50aac739eb49a1a21
Gurvan Biguet--Kerloc'h <gurvan.biguet-kerloch@epita.fr>
2026-05-15 17:14:02 +0200

    fix: update dashboard to nixos25.11

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   description = "Homelab infrastructure";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-25-11.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, nixpkgs-25-11 }:
     {
-      nixosConfigurations = import ./hosts { inherit nixpkgs; };
+      nixosConfigurations = import ./hosts { inherit nixpkgs nixpkgs-25-11; };
     };
 }
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,7 @@
-{ nixpkgs }:
+{ nixpkgs, nixpkgs-25-11 }:
 {
   nix-builder = import ./nix-builder { inherit nixpkgs; };
   k3s-master = import ./k3s-master { inherit nixpkgs; };
   k3s-worker-1 = import ./k3s-worker-1 { inherit nixpkgs; };
-  dashboard = import ./dashboard { inherit nixpkgs; };
+  dashboard = import ./dashboard { nixpkgs = nixpkgs-25-11; };
 }
